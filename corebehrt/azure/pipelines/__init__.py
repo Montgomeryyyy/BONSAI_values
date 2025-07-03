1745c18dfbac7ca5af65b606cd5f16005965eb9e
Montgomeryyyy <maria.elk.mont@gmail.com>
2025-07-03 14:48:34 +0200

    added option

--- corebehrt/azure/pipelines/__init__.py
+++ corebehrt/azure/pipelines/__init__.py
@@ -1,5 +1,7 @@
 from corebehrt.azure.pipelines.E2E import E2E
+from corebehrt.azure.pipelines.E2E_full import E2E_full
 from corebehrt.azure.pipelines.FINETUNE import FINETUNE
 from corebehrt.azure.pipelines.E2E_decoder import E2E_DECODER
 
-PIPELINE_REGISTRY = [E2E, FINETUNE, E2E_DECODER]
+
+PIPELINE_REGISTRY = [E2E, E2E_full, FINETUNE, E2E_DECODER]
