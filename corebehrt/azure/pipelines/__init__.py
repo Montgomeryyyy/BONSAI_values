bd542a8f252ba22c4f740224e3d07e8caee236c7
Montgomeryyyy <maria.elk.mont@gmail.com>
2025-07-05 13:02:26 +0200

    xgb pipeline

--- corebehrt/azure/pipelines/__init__.py
+++ corebehrt/azure/pipelines/__init__.py
@@ -1,7 +1,8 @@
 from corebehrt.azure.pipelines.E2E import E2E
 from corebehrt.azure.pipelines.E2E_full import E2E_full
 from corebehrt.azure.pipelines.FINETUNE import FINETUNE
+from corebehrt.azure.pipelines.E2E_XGB import E2E_XGB
 from corebehrt.azure.pipelines.E2E_decoder import E2E_DECODER
 
 
-PIPELINE_REGISTRY = [E2E, E2E_full, FINETUNE, E2E_DECODER]
+PIPELINE_REGISTRY = [E2E, E2E_full, FINETUNE, E2E_DECODER, E2E_XGB]
